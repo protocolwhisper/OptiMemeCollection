d026bef43962d34abe13249721ecfb2969c77a32
jovi <77895278+joaovwfreire@users.noreply.github.com>
2022-11-14 15:02:42 -0300

    Frontend Settings update

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -16,14 +16,14 @@ const { chains, provider, webSocketProvider } = configureChains(
     chain.optimism,
     chain.arbitrum,
     ...(process.env.NEXT_PUBLIC_ENABLE_TESTNETS === 'true'
-      ? [chain.goerli, chain.kovan, chain.rinkeby, chain.ropsten]
+      ? [chain.goerli, chain.optimismGoerli]
       : []),
   ],
   [
     alchemyProvider({
       // This is Alchemy's default API key.
       // You can get your own at https://dashboard.alchemyapi.io
-      apiKey: '_gg7wSSi0KMBsdKnGVfHDueq6xMB9EkC',
+      apiKey: process.env.NEXT_PUBLIC_ALCHEMY_API_KEY,
     }),
     publicProvider(),
   ]
